9672619fafb2841ee3e12c3b367ca2562ddbcaad
TEST2-jpg <agapdag@gmail.com>
2022-06-08 18:06:20 +0800

    Add profile title

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,12 +9,12 @@ class Profile extends Component {
         location: "123 Anywhere St. XXX City 1233",
         email: "hello@gmail.com",
         profileInfo: "Assists the department head in carrying out digital marketing companies works closely with the marketing head for digital production",
-        
+        website: "zzzz.com"
     }
   }
 
   render() {
-    const {name, title, location, email, profileInfo} = this.state;
+    const {name, title, location, email, profileInfo, website} = this.state;
     return (
       <div className='profile'>
         <h1>{this.props.title}</h1>
@@ -22,6 +22,8 @@ class Profile extends Component {
         <div className='title'>{title}</div>
         <div className='location'>{location}</div>
         <div className='email'>{email}</div>
+        <div className='website'>{website}</div>
+        <div className='profileTitle'>PROFILE</div>
         <div className='profileInfo'>{profileInfo}</div>
       </div>
     );
