5e64fc4d6a71b7b1d391cd31fe6871371ebd6dd3
TEST2-jpg <agapdag@gmail.com>
2022-06-08 16:34:11 +0800

    Boilerplate

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import GenInfo from "./components/GenInfo";
+// import "./style.css"
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+render() {
+    return (
+      <div>
+        <GenInfo title="asadsdasd"/>
+      </div>
+    );
+  }
+}
+
+export default App
