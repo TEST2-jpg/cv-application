f2d5c6b28e5668b6445090ba7635c62618317757
TEST2-jpg <agapdag@gmail.com>
2022-06-09 13:25:51 +0800

    add profile styles

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import Profile from "./components/Profile";
 import "./style.css"
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true
     }
+    this.showBtn = this.showBtn.bind(this);
+  }
+  showBtn() {
+    this.setState({ hidden: !this.state.hidden })
+    console.log('clicked', this.state.hidden)
 
-render() {
+  }
+  render() {
     return (
       <div className="main">
-        <Profile title="asadsdasd"/>
+        <div className="prof" onMouseEnter={this.showBtn} onMouseLeave={this.showBtn} onClick={()=> console.log(12)}>
+          <Profile title="asadsdasd" show={!this.state.hidden}/>
+        </div>
         <div className="work"></div>
       </div>
     );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,16 +9,18 @@ class Profile extends Component {
         location: "123 Anywhere St. XXX City 1233",
         email: "hello@gmail.com",
         profileInfo: "Assists the department head in carrying out digital marketing companies works closely with the marketing head for digital production",
-        website: "zzzz.com"
+        website: "zzzz.com",
+        photoURL: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
     }
   }
-
   render() {
-    const {name, title, location, email, profileInfo, website} = this.state;
+    let btnImg = <img className='edit' src="https://www.svgrepo.com/show/46991/edit.svg"></img>
+    const {name, title, location, email, profileInfo, website, photoURL} = this.state;
     return (
       <div className='profile'>
-        <h1>{this.props.title}</h1>
-        <div className='name'>{name}</div>
+        {/* <h1>{this.props.title}</h1> */}
+        <img src={photoURL} alt='UserPhoto' className='userImg'></img>
+        <div className='name'>{name} <span>{this.props.show ? <button>{btnImg}</button> : null}</span></div>
         <div className='title'>{title}</div>
         <div className='location'>{location}</div>
         <div className='email'>{email}</div>
