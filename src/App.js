05d9ebd47e5df4b4fe49fec7504ed9c379262440
TEST2-jpg <agapdag@gmail.com>
2022-06-10 17:04:53 +0800

    add btn hover and experience section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Experience from "./components/Experience";
 import Profile from "./components/Profile";
 import "./style.css"
 
@@ -6,14 +7,18 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hidden: true
+      hidden: true,
+      expHidden: true,
     }
     this.showBtn = this.showBtn.bind(this);
+    this.expShowBtn = this.expShowBtn.bind(this);
+
   }
   showBtn() {
     this.setState({ hidden: !this.state.hidden })
-    console.log('clicked', this.state.hidden)
-
+  }
+  expShowBtn() {
+    this.setState({ expHidden: !this.state.expHidden })
   }
   render() {
     return (
@@ -21,7 +26,9 @@ class App extends Component {
         <div className="prof" onMouseEnter={this.showBtn} onMouseLeave={this.showBtn} onClick={()=> console.log(12)}>
           <Profile title="asadsdasd" show={!this.state.hidden}/>
         </div>
-        <div className="work"></div>
+        <div className="work" onMouseEnter={this.expShowBtn} onMouseLeave={this.expShowBtn} onClick={()=> console.log(132)} >
+          <Experience show={!this.state.expHidden}/>
+        </div>
       </div>
     );
   }
