f86e3c3daf8e9f881312c83dcaf184ec2ce6858e
TEST2-jpg <agapdag@gmail.com>
2022-06-10 18:12:46 +0800

    Add forms

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,23 +4,63 @@ class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        name: "Cia Rodriguez",
-        title: "Graphic Designer",
-        location: "123 Anywhere St. XXX City 1233",
-        email: "hello@gmail.com",
-        profileInfo: "Assists the department head in carrying out digital marketing companies works closely with the marketing head for digital production",
-        website: "zzzz.com",
-        photoURL: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+      name: "Cia Rodriguez",
+      title: "Graphic Designer",
+      location: "123 Anywhere St. XXX City 1233",
+      email: "hello@gmail.com",
+      profileInfo: "Assists the department head in carrying out digital marketing companies works closely with the marketing head for digital production",
+      website: "zzzz.com",
+      photoURL: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      showForm: false,
+      submitt: false,
     }
+    this.showForm = this.showForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
   }
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({ submitt: !this.state.submitt })
+    console.log(event.target.location.value)
+  }
+  showForm() {
+    this.setState({ showForm: !this.state.showForm })
+    console.log(this.state.showForm, 'monkey')
+  }
+
   render() {
-    let btnImg = <img className='edit' src="https://www.svgrepo.com/show/46991/edit.svg"></img>
-    const {name, title, location, email, profileInfo, website, photoURL} = this.state;
+    let btnImg = <img className='edit' src="https://www.svgrepo.com/show/46991/edit.svg" alt='edit profile'></img>
+    const { name, title, location, email, profileInfo, website, photoURL } = this.state;
     return (
       <div className='profile'>
+        <div>
+          {this.state.showForm && (
+            <div className='forms'>
+              <form className='profileForm' onSubmit={this.handleSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" value={name} onChange={this.handleChange} placeholder="Enter your full name" id="name"></input>
+                <label htmlFor="title">Title</label>
+                <input type="text" name="title" value={title} onChange={this.handleChange} placeholder="Enter your job title" id="title"></input>
+                <label htmlFor="address">Address</label>
+                <input type="text" name="location" value={location} onChange={this.handleChange} placeholder="Enter your address" id="address"></input>
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" value={email} onChange={this.handleChange} placeholder="Enter your email" id="email"></input>
+                <label htmlFor="profInfo">Linkedin</label>
+                <input type="text" name="website" value={website} onChange={this.handleChange} placeholder="Enter your Linkedin proile" id="linkedin"></input>
+                <label htmlFor="linkedin">Profile Info</label>
+                <input type="text" name="profileInfo" value={profileInfo} onChange={this.handleChange} placeholder="Profile info" id="profInfo"></input>
+                <button className='submit'>Submit</button>
+              </form>
+            </div>
+          )}
+        </div>
         {/* <h1>{this.props.title}</h1> */}
         <img src={photoURL} alt='UserPhoto' className='userImg'></img>
-        <div className='name'>{name} <span>{this.props.show ? <button>{btnImg}</button> : null}</span></div>
+        <div className='name'>{name}<span>{this.props.show ? <button className='profileBtn' onClick={this.showForm}>{btnImg}</button> : null}</span></div>
         <div className='title'>{title}</div>
         <div className='location'>{location}</div>
         <div className='email'>{email}</div>
