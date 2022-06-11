30a5ebf52a7c07c8af948610f2c13c61dfd989dd
TEST2-jpg <agapdag@gmail.com>
2022-06-11 19:06:51 +0800

    Removed hover btn logic

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React, { Component } from "react";
-import Experience from "./components/Experience";
+import Experienceforms from "./components/Experienceforms";
 import Profile from "./components/Profile";
+
 import "./style.css"
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      hidden: true,
-      expHidden: true,
-    }
-    this.showBtn = this.showBtn.bind(this);
-    this.expShowBtn = this.expShowBtn.bind(this);
 
   }
-  showBtn() {
-    this.setState({ hidden: !this.state.hidden })
-  }
-  expShowBtn() {
-    this.setState({ expHidden: !this.state.expHidden })
-  }
+
   render() {
     return (
       <div className="main">
-        <div className="prof" onMouseEnter={this.showBtn} onMouseLeave={this.showBtn} onClick={()=> console.log(12)}>
-          <Profile title="asadsdasd" show={!this.state.hidden}/>
+        <div className="prof">
+          <Profile/>
         </div>
-        <div className="work" onMouseEnter={this.expShowBtn} onMouseLeave={this.expShowBtn} onClick={()=> console.log(132)} >
-          <Experience show={!this.state.expHidden}/>
+        <div className="work">
+          <Experienceforms/>
         </div>
       </div>
     );
