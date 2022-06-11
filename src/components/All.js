9fd0912d464e92ebb4257166b655686086d04a1b
TEST2-jpg <agapdag@gmail.com>
2022-06-11 13:56:49 +0800

    Add work section render

---
+++ src/components/All.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class All extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='exptitle'>WORK EXPERIENCE</div>
+        <hr className='line--exp'></hr>
+        {this.props.data.map((data, key) => {
+          return <div key={key}>
+            <div className='title--exp'>{data.title}</div>
+            <div className='spaced'>
+              <span className='comp--exp'>{data.company}</span>
+              <span className='date--exp'>{data.date}</span>
+            </div>
+            <div className='tasks--exp'>{data.tasks}</div>
+
+          </div>
+        })}
+      </div>
+    );
+  }
+}
+
+export default All;
